8ae9387cd04383240d1b7fe3a2a897cbd5be9c27
Brandon Nozaki Miller <brandon@diginow.it>
2014-10-28 03:37:37 -0700

    completed basic module and example

--- bluetooth-programmer.js
+++ bluetooth-programmer.js
@@ -1,7 +1,8 @@
 var serialPort = require("serialport");
 var SerialPort = serialPort.SerialPort;
 var config={
-    baud:9600
+    baud:9600,
+    delay:950
 }
 
 var availableBTPorts={};
@@ -11,6 +12,7 @@ var portData={
     testingPort : 0,
     testingBaud : 0,
     testDuration: 750,
+    forcedBaud  : false,
     testBauds   : [
         1200,
         2400,
@@ -25,16 +27,179 @@ var portData={
 
 var testTimeout=false;
 
-function findBTDevices(baud){
-    if(!baud)
-        baud=config.baud;
+var BTUSB=false;
+var onFound=false;
 
+function connectToBTDevice(port, onOpen){
+    if(!port)
+        return false;
+    if(!port.comName)
+        return false;
+    
+    if(!port.baud)
+        port.baud=9600;
+    
+    var BTConnection=new SerialPort(
+        port.comName,
+        {
+            baudrate: port.baud,
+            parser  : serialPort.parsers.readline("OK")
+        }
+    );
+
+    function BTCommand(data){
+        BTConnection._BTCommands.push(data);
+        if(BTConnection._BTCommands.length<2)
+            BTConnection.write(data);
+        
+        if(BTConnection.commandTimer)
+            return;
+        
+        BTConnection.commandTimer=true;
+        (
+            function(BTConnection){
+                setTimeout(
+                    function(){
+                        runCommand.apply(BTConnection);
+                    },
+                    config.delay
+                );
+            }
+        )(BTConnection);
+    }
+
+    BTConnection.BTPin=setBTPin;
+    BTConnection.BTBaud=setBTBaud;
+    BTConnection.BTName=setBTName;
+    BTConnection.BTParity=setBTParity;
+    BTConnection.BTTest=BTTest;
+    BTConnection.BTVersion=getBTVersion;
+    
+    BTConnection.BTCommand=BTCommand;
+    BTConnection._BTCommands=[];
+    BTConnection.runCommand=runCommand;
+    
+    if(!onOpen)
+        return BTConnection;
+    
+    BTConnection.on(
+        "open",
+        onOpen
+    );
+
+    return BTConnection;
+}
+
+function runCommand(){
+    this._BTCommands.shift();
+    if(this._BTCommands.length<1){
+        this.commandTimer=true;
+        return;
+    }
+    
+    //console.log(this._BTCommands[0])
+    this.write(this._BTCommands[0]);
+    (
+        function(BTConnection){
+            setTimeout(
+                function(){
+                    runCommand.apply(BTConnection);
+                },
+                config.delay
+            );
+        }
+    )(this);
+}
+
+function setBTPin(pin){
+    this.BTCommand("AT+PIN"+pin);
+}
+
+function setBTBaud(baud){
+    baud=Number(baud);
+    
+    switch(baud){
+        case 1 :
+        case 2 :
+        case 3 :
+        case 4 :
+        case 5 :
+        case 6 :
+        case 7 :
+        case 8 :
+            //use as native value
+            break;
+        case 1200 :
+            baud=1;
+            break;
+        case 2400 :
+            baud=2;
+            break;
+        case 4800 :
+            baud=3;
+            break;
+        case 9600 :
+            baud=4;
+            break;
+        case 19200 :
+            baud=5;
+            break;
+        case 38400 :
+            baud=6;
+            break;
+        case 57600 :
+            baud=7;
+            break;
+        case 115200 :
+            baud=8;
+            break;
+        default :
+            return;
+    }
+    
+    this.BTCommand("AT+BAUD"+baud);
+}
+
+function setBTName(name){
+    this.BTCommand("AT+NAME"+name);
+}
+
+function setBTParity(parity){
+    parity=parity.toLowerCase();
+    
+    switch(parity){
+        case 'none' :
+            parity='N';
+            break;
+        case 'even' :
+            parity='E';
+            break;
+        case 'odd' :
+            parity='O';
+            break;
+        default :
+            return;
+    }
+    
+    this.BTCommand("AT+P"+parity);
+}
+
+function BTTest(){
+    this.BTCommand("AT");
+}
+
+function getBTVersion(){
+    this.BTCommand("AT+VERSION");
+}
+
+function findBTDevices(callback, baud){
     closeBTUSB();
 
-    portData.baud=baud;
-
+    onFound=callback;
+    
     availableBTPorts={};
-
+    
+    portData.forcedBaud=baud;
     portData.ports=[];
     portData.testingPort=0;
     portData.testingBaud=-1;
@@ -43,7 +208,7 @@ function findBTDevices(baud){
 
     serialPort.list(
         function (err, ports) {
-            console.log(ports); //this is only logging the single most recently connected serial port.
+            //console.log(ports); //this is only logging the single most recently connected serial port.
             portData.ports=ports;
             detectBTDevices();
         }
@@ -54,7 +219,7 @@ function detectBTDevices(){
     closeBTUSB();
     portData.testingBaud++;
 
-    if(portData.testingBaud>=portData.testBauds.length){
+    if(portData.testingBaud>=portData.testBauds.length || (portData.testingBaud>0 && portData.forcedBaud)){
         portData.testingBaud=0;
         portData.testingPort++;
     }
@@ -62,49 +227,74 @@ function detectBTDevices(){
     if(portData.testingPort>=portData.ports.length){
         clearTimeout(testTimeout);
         portData.testingPort=-1;
-        console.log(availableBTPorts);
+        if(!onFound)
+            return;
+        
+        onFound(availableBTPorts);
         return;
     }
+    
+    var baud=(
+        (portData.forcedBaud)? 
+            portData.forcedBaud
+                :
+            portData.testBauds[
+                portData.testingBaud
+            ]
+    );
 
+    /*
     console.log(
         portData.ports[
             portData.testingPort
         ].comName,
-        portData.testBauds[
-            portData.testingBaud
-        ]
+        baud
     );
-
+    */
+   
     BTUSB = new SerialPort(
         portData.ports[
             portData.testingPort
         ].comName,
         {
-            baudrate: portData.testBauds[
-                portData.testingBaud
-            ],
+            baudrate: baud,
             parser  : serialPort.parsers.readline("OK")
-        },
-        function () {
+        }
+    );
+
+    BTUSB.on(
+        'open',
+        function(){
+            testTimeout=setTimeout(
+                detectBTDevices,
+                portData.testDuration
+            );
+        
             BTUSB.on(
                 "data",
                 function (data) {
                     closeBTUSB();
-                    console.log('#',data)
+                    //console.log('#',data)
                     var BTDevice=portData.ports[
                         portData.testingPort
                     ];
 
-                    console.log(portData.testingPort,BTDevice.comName);
-
+                    //console.log(portData.testingPort,BTDevice.comName);
+                    
+                    var baud=(
+                        (portData.forcedBaud)? 
+                            portData.forcedBaud
+                                :
+                            portData.testBauds[
+                                portData.testingBaud
+                            ]
+                    );
+                    
+                    BTDevice.baud=baud;
+                    
                     availableBTPorts[
                         BTDevice.comName
-                    ]={
-                        port:BTDevice,
-                        baud:portData.testBauds[
-                            portData.testingBaud
-                        ]
-                    };
+                    ]=BTDevice;
 
                     clearTimeout(testTimeout);
                     portData.testingBaud=-1;
@@ -112,12 +302,6 @@ function detectBTDevices(){
                     detectBTDevices();
                 }
             );
-
-            testTimeout=setTimeout(
-                detectBTDevices,
-                portData.testDuration
-            );
-
 
             BTUSB.write("AT");
         }
@@ -132,4 +316,12 @@ function closeBTUSB(){
     }
 }
 
-findBTDevices(38400);
+module.exports={
+    data:{
+        config:config,
+        testData:portData,
+        availableBTPorts:availableBTPorts
+    },
+    find    : findBTDevices,
+    connect : connectToBTDevice
+}
---
+++ examples/reprogram-bluetooth.js
@@ -0,0 +1,84 @@
+var BTP=require('../bluetooth-programmer.js');
+var BTDevices=[];
+
+//search all connected ports for Bluetooth devices and discover their baud rate.
+BTP.find(foundDevices);
+console.log('Searching for any BlueTooth devices connected via USB or serial')
+
+
+function foundDevices(devices){
+    var list=Object.keys(devices);
+    BTDevices=devices;
+    console.log('Found BT Devices :\n####>',devices);
+    
+    if(!list[0])
+        return;
+    
+    BTP.connect(devices[list[0]],connectedToBT);
+}
+
+function connectedToBT(){
+    this.on(
+        "data",
+        gotData
+    );
+    this.on(
+        "close",
+        completeBTProgramming
+    );
+    this.on(
+        "disconnect",
+        completeBTProgramming
+    );
+    this.on(
+        "error",
+        function(err){
+            console.log(err);
+        }
+    );
+    
+    console.log('setting Name');
+    this.BTName('DNZ38400');
+    console.log('setting Pin');
+    this.BTPin('1314');
+    console.log('setting parity');
+    this.BTParity('None');
+    console.log('setting baud');
+    this.BTBaud(38400);
+    this.BTTest();//follow up with an extra Test to fetch the last message
+    
+    //Make sure BT doesn't hang when baud rate changed. And double check to make sure baud rate is correct.
+    (
+        function(BT){
+            setTimeout(
+                function(){
+                    try{
+                        BT.close();
+                    }catch(err){
+                        //do nothing because BT is already closed
+                    }
+                    console.log('Double checking modifications, searching for any BT Devices connected via USB or serial at the new baud rate.');
+                    BTP.find(modifiedDevices,38400);
+                },
+                7500
+            );
+        }
+    )(this);
+}
+
+function modifiedDevices(devices){
+    console.log('\n\n########## Modification Complete #############\n\n','found',Object.keys(devices).length,'BT devices ###>',devices);
+}
+
+function completeBTProgramming(){
+    console.log('finished board 0');
+}
+function gotData(data){
+    console.log(data);
+    if(data=="38400")
+        this.close();
+}
+
+function completeBTProgramming(){
+    console.log('finished board 0');
+}
